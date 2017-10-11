716b467cc0da2bf6d163cec01b19d61c6744454c
dotJS <justdotJS@icloud.com>
2017-10-11 16:32:38 +0800

    ugh maffie

--- rowboat/plugins/utilities.py
+++ rowboat/plugins/utilities.py
@@ -30,7 +30,7 @@ from rowboat.constants import (
     STATUS_EMOJI, SNOOZE_EMOJI, GREEN_TICK_EMOJI, GREEN_TICK_EMOJI_ID,
     EMOJI_RE, USER_MENTION_RE, YEAR_IN_SEC, CDN_URL
 )
-from google import *
+from google import google
 
 def search_google_images(query):
     return google.search_images(query)
